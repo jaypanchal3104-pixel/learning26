275d9f23a551cd131aa512fa9fcab1b19563f765
jaypanchal3104-pixel <jaypanchal3104@gmail.com>
2026-02-09 22:02:31 +0530

    1 to 1 and 1 to many classes

--- learning26/student/admin.py
+++ learning26/student/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Student,Product
+from .models import Student,Product,car,StudentProfile,Category,Service,intern,internDetails,teacher,subject
 # Register your models here.
 
 admin.site.register(Student)
 admin.site.register(Product)
+admin.site.register(car)
+admin.site.register(StudentProfile)
+admin.site.register(Category)
+admin.site.register(Service)
+admin.site.register(intern)
+admin.site.register(internDetails)
+admin.site.register(teacher)
+admin.site.register(subject)
--- learning26/student/models.py
+++ learning26/student/models.py
@@ -23,3 +23,102 @@ class Product(models.Model):
     
     class Meta:
         db_table = "product"
+class car(models.Model):
+    carName=models.CharField(max_length=50)
+    companyName=models.CharField(max_length=50)
+    carColour=models.CharField(max_length=20)
+
+    class Meta:
+        db_table = "car"
+
+#1-1 one std=one stdProfile
+class StudentProfile(models.Model):
+    hobbies =(("reading","reading"),("travel","travel"),("music","music"))
+    #studentPrilfe id --> pk create auto...
+    studentId = models.OneToOneField(Student,on_delete=models.CASCADE)
+    studentHobbies = models.CharField(max_length=100,choices=hobbies)
+    studentAddress = models.CharField(max_length=100)
+    studentPhone = models.CharField(max_length=10)
+    studentGender = models.CharField(max_length=10)
+    studentDOB = models.DateField()
+    
+    class Meta:
+        db_table = "studentprofile"
+    def _str_(self):
+        return self.studentId.studentName    
+
+# for 1 to Many:-
+#category --> #service
+
+class Category(models.Model):
+    categoryName = models.CharField(max_length=100)
+    categoryDescription = models.TextField()
+    categoryStatus = models.BooleanField(default=True)
+    
+    class Meta:
+        db_table = "category"
+
+    def _str_(self):
+        return self.categoryName    
+
+class Service(models.Model):
+    serviceName = models.CharField(max_length=100)
+    serviceDescription = models.TextField()
+    servicePrice = models.IntegerField()
+    serviceStatus = models.BooleanField(default=True)
+    #after table creation adding new field
+    discount = models.IntegerField(null=True)
+    categoryId = models.ForeignKey(Category,on_delete=models.CASCADE)
+
+    
+    class Meta:
+        db_table = "service"
+
+    def _str_(self):
+        return self.serviceName    
+    
+
+class intern(models.Model):
+    internName=models.CharField(max_length=100)
+    internAge=models.IntegerField()
+    Department=models.CharField(max_length=100)
+
+    class Meta:
+        db_table = 'intern'
+    
+    def _str_(self):
+        return self.internName
+    
+class internDetails(models.Model):
+    internID=models.OneToOneField(intern,on_delete=models.CASCADE)
+    internPhone = models.CharField(max_length=10)
+    internGender = models.CharField(max_length=10)
+
+    class Meta:
+        db_table = 'internDetails'
+
+    def _str_(self):
+        return self.internID.internName
+    
+class teacher(models.Model):
+    teacherName=models.CharField(max_length=100)
+    phoneNo=models.IntegerField(max_length=10)
+    Email=models.EmailField()
+    
+    class Meta:
+        db_table = 'teacher'
+    
+    def _str_(self):
+        return self.teacherName
+    
+class subject(models.Model):
+    subject=(("BEE","BEE"),("BME","BME"),("M-2","M-2"))
+    subName=models.CharField(max_length=100,choices=subject)
+    subID=models.IntegerField(max_length=25)
+    teacher = models.ForeignKey(teacher,on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'subject'
+
+    def _str_(self):
+        return self.teacher.teacherName
